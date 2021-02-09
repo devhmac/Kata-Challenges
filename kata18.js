c8e649520b54c8f3e281eea088f328f9cb9dc4c9
Devin MacGillivray <devhmac@gmail.com>
2021-02-09 17:51:40 +0000

    Randon number generator guessing game complete!

--- kata18.js
+++ kata18.js
@@ -5,25 +5,25 @@ let answer
 //console.log("You answered: " + answer);
 console.log("Welcome to the random number guessing game!");
 console.log("If you would like to exit at any time, please enter the command:'exit' ")
-let num = 5
+let num = Math.floor((Math.random() * 100) + 1)
 let guesses=[]
 
 while (answer !== num) {
   answer = prompt("Guess a number: ");
   if (answer === 'exit') {
+    console.log("Goodbye.")
     break;
   }
   if (answer == num) {
-    console.log("Your guess was correct!");
     guesses.push(answer);
+    console.log(`You guessed correctly! You took ${guesses.length} attempts` );
+    
     break;
   }
 
  if (guesses.includes(answer)){
-   console.log("You have already guessed that, guess again.")
- }
-
-  if (answer < num) {
+   console.log("You have already guessed that number, guess again.");
+ } else if (answer < num) {
     console.log("your Guess was too low");
     guesses.push(answer);
   } else if (answer > num) {
@@ -33,7 +33,6 @@ while (answer !== num) {
     console.log("Your guess was invalid, please guess a number.");
   }
   
-
 }
 
-console.log(`You guessed correctly! You took ${guesses.length} attempts` );
+
